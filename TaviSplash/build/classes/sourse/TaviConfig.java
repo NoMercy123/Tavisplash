9437c1ad09bf3d9b044b9f9c33d8988d334177b3
OldTeaOwl <Levikhindenis@rambler.ru>
2016-05-23 00:15:20 +0300

    <Незначительные поправки>

--- TaviSplash/build/classes/sourse/TaviConfig.java
+++ TaviSplash/build/classes/sourse/TaviConfig.java
@@ -1,5 +1,6 @@
 package sourse;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Toolkit;
@@ -18,8 +19,10 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.LineBorder;
 
 /**
 * @author Den
@@ -44,10 +47,13 @@ JLabel frase = new JLabel("Фразы: ");
 JLabel skin = new JLabel("Скин: ");
 JButton run = new JButton("Готово");
 JButton Exit = new JButton("Выход");
+JButton Back = new JButton("Готово");
+JButton Me = new JButton("OldTeaOwl");
 JLabel path = new JLabel("Октавия");
 JLabel colormark = new JLabel("Металлик");
 JLabel frasemark = new JLabel("<default>");
 JLabel how2use = new JLabel("How2Use: Ctrl+Spase");
+JLabel project = new JLabel(new ImageIcon("D:/Data/AboutOctavia.png"));
 JMenuBar test = new JMenuBar();
 JMenu colormenu = new JMenu("Цвет");
 JMenu frasemenu = new JMenu("Фразы");
@@ -65,6 +71,7 @@ JMenuItem Rarity = new JMenuItem("Рарити");
 JMenuItem Applejack = new JMenuItem("Эпплджек");
 JMenuItem Raibow = new JMenuItem("Рейнбоу Дэш");
 JMenuItem Octavia = new JMenuItem("Октавия");
+JTextArea abouttext = new JTextArea();
 Exit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 System.exit(0);
@@ -90,6 +97,15 @@ about.addMouseListener(new MouseAdapter() {
 aboutframe.setVisible(true);
   }
 });
+Back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+aboutframe.setVisible(false);
+            }
+        });
+Me.setBounds(320,18,120,20);
+project.setBounds(10,10,200,300);
+Back.setBounds(200,325,80,40);
+abouttext.setBounds(220,10,250,300);
 how2use.setBounds(247,143,150,20);
 Tavi.setBounds(230,10,150,150);
 test.setBounds(0,0,400,20);
@@ -111,7 +127,22 @@ frasemark.setFont(font);
 metallic.setFont(font);
 green.setFont(font);
 yellow.setFont(font);
+abouttext.setFont(font);
+abouttext.setEditable(false);
+abouttext.setEnabled(false);
+abouttext.setLineWrap(true);
+abouttext.setWrapStyleWord(true);
+abouttext.setDisabledTextColor(Color.black);
+project.setBorder(new LineBorder(Color.black));
 how2use.setFont(new Font("Times New Roman", Font.PLAIN, 10));
+abouttext.setText("Разработчик: "
+                + "\nНазвание: Tavisplash"
+                + "\nДата создания: 22.05.16, 23:54"
+                + "\nЯзык: Русский"
+                + "\n\nОписание: Простая бесплатная утилита."
+                + " Её цель - демонстрация заметок в реальном времени по нажатию "
+                + "клавиш Cntr+Spase, или по таймеру. Можно использовать в процессе "
+                + "видеозахвата при создании роликов.");
 colormenu.add(pink);
 colormenu.add(metallic);
 colormenu.add(green);
@@ -139,12 +170,18 @@ config.add(Exit);
 config.add(color);
 config.add(frase);
 config.add(skin);
-aboutframe.setBounds((hor-600)/2,(vert-400)/2,600,400);
+aboutframe.add(Me);
+aboutframe.add(abouttext);
+aboutframe.add(project);
+aboutframe.add(Back);
+aboutframe.setResizable(false);
+aboutframe.setLayout(null);
+aboutframe.setVisible(false);
+aboutframe.setBounds((hor-500)/2,(vert-400)/2,500,400);
 config.setBounds((hor-400)/2,(vert-200)/2,400,200);
 config.setLayout(null);
 config.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+config.setResizable(false);
 config.setVisible(true);
-aboutframe.setLayout(null);
-aboutframe.setVisible(false);
 }
 }
